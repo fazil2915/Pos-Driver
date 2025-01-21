9a9f7b72688cf5063a9bdf7d562245da1a8789d3
fazil2915 <fazilrah9846@gmail.com>
2025-01-21 12:56:08 +0530

    added to my branch

--- src/main/java/com/example/pos_driver/Controller/DriverController.java
+++ src/main/java/com/example/pos_driver/Controller/DriverController.java
@@ -1,14 +1,18 @@
 package com.example.pos_driver.Controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DriverController {
 
+    private static final Logger logger = LoggerFactory.getLogger(DriverController.class);
+
     @GetMapping("/test")
-    public String firstName(){
+    public String firstName() {
+        logger.info("requesting to  pos readed");
         return "test api";
     }
-
 }
