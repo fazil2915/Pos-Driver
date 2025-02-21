772b9e80324303dcf7e71e0fe38f26ffafac0f07
yato <amalsunny4466@gmail.com>
2025-02-21 11:13:19 +0530

    add new updates (cvv,trim track 2)

--- src/main/java/com/example/pos_driver/PosDriverApplication.java
+++ src/main/java/com/example/pos_driver/PosDriverApplication.java
@@ -4,11 +4,23 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.io.File;
+
 @SpringBootApplication
 @EnableScheduling
 public class PosDriverApplication {
 
 	public static void main(String[] args) {
+
+		String externalConfigPath = "file:C:/Vita-Pos/VITA_POS/application.properties";
+		File externalFile = new File("C:/Vita-Pos/VITA_POS/application.properties");
+
+		if (externalFile.exists()) {
+//			System.out.println("External properties file found: " + externalConfigPath);
+			System.setProperty("spring.config.location", externalConfigPath);
+		} else {
+			System.out.println("Using default properties from resources folder.");
+		}
 		SpringApplication.run(PosDriverApplication.class, args);
 	}
 
